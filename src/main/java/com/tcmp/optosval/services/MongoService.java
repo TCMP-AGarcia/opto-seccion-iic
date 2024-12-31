c30f72ae911d839bca6e99a0703a0bea0f9f91bf
TCMP-AGarcia <alan.garcia@tcmpartners.com>
2024-12-31 11:30:21 -0600

    311224V5-Modificacion a MongoService

--- src/main/java/com/tcmp/optosval/services/MongoService.java
+++ src/main/java/com/tcmp/optosval/services/MongoService.java
@@ -10,7 +10,9 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Slf4j
@@ -23,40 +25,57 @@ public class MongoService {
         // Get the MongoDB database
         MongoDatabase database = mongoTemplate.getDb();
         MongoCollection<Document> collection = database.getCollection("Realtime");
+        MongoCollection<Document> collectionCF = database.getCollection("Cashflow");
 
         // Create the projection to fetch only the required fields
-        Document projection = new Document("TradeMessage.trade.tradeHeader.tradeIdentifiers.tradeId.id", 1)
-                .append("CashflowMessage.cashflowDetails.actualCashflowDate", 1)
-                .append("TradeMessage.trade.parties.counterparty.partyName", 1)
-                .append("TradeMessage.trade.product_FXOption.underlyingCurrencyCode", 1)
-                .append("TradeMessage.trade.product_FXOption.underlyingAmount", 1)
-                .append("TradeMessage.trade.product_FXOption.baseCurrency", 1)
-                .append("TradeMessage.trade.product_FXOption.baseAmount", 1)
-                .append("CashflowMessage.cashflowDetails.cashflowCurrencyCode", 1)
-                .append("CashflowMessage.cashflowDetails.cashflowAmount", 1)
+        Document projection = new Document(
+
+                // TODO CONT ANEXO B
+
+                // NU_ID RUTA trade.id
+                "TradeMessage.trade.tradeHeader.tradeIdentifiers.tradeId.id", 1)
+                // FECHA
+                .append("TradeMessage.trade.tradeHeader.tradeDate", 1)
+                // TODO NU_PE_EJE Pending to create
+
+                // FEINOP_CO
+                .append("TradeMessage.trade.product_FXOption.startDate",1)
+                //FEVEOP_CO
+                .append("TradeMessage.trade.product_FXOption.endDate",1)
+                // TODO SUBY_CO CATALOGO ANEXO F
+
+                // TODO CVE_TIT_C ANEXO AF
                 .append("TradeMessage.trade.product_FXOption.underlyingInstrumentName", 1)
-                .append("CashflowMessage.cashflowDetails.cashflowCurrencyCode", 1)
-                .append("_id", 0); // Exclude the _id field
+                // PRECIOEJER_C
+                .append("TradeMessage.trade.product_FXOption.strikeRate", 1)
+                // PRE_SUP
+                .append("TradeMessage.trade.product_FXOption.barrierFeature.barrierUpRate", 1)
+                // PRE_INF
+                .append("TradeMessage.trade.product_FXOption.barrierFeature.barrierDownRate", 1)
+                // INST_LEI
+                .append("TradeMessage.trade.parties.counterparty.partyLei", 1)
+                // UTI
+                .append("TradeMessage.trade.tradeHeader.tradeIdentifiers.uniqueTransactionId", 1)
+                // TODO IDENTIFICADOR VanillaOption de SECCIONII se deja vacío
+
+        .append("_id", 0);  // Exclude the _id field
+
+        Document projectionCF = new Document("_id", 1)
+                .append("JournalEntryMessage.CashflowMessage.cashflowDetails.0.cashflowAmount", 1);
 
         // Query the collection and apply the projection
-        List<Document> results = collection.find(new Document()).projection(projection).into(new ArrayList<>());
+        List<Document> resultsRT = collection.find(new Document()).projection(projection).into(new ArrayList<>());
+        List<Document> resultsCF = collectionCF.find(new Document()).projection(projectionCF).into(new ArrayList<>());
 
         // Process the results
-        List<Document> formattedResults = new ArrayList<>();
-        for (Document doc : results) {
-            // Create a new field combining underlyingInstrumentName and cashflowCurrencyCode
-            String combinedField = "dummy" + "/" +
-                    "string";
-            doc.append("combinedField", combinedField);
-
-            // Add the processed document to the results
-            formattedResults.add(doc);
-        }
-
-        // Log the formatted results
-        //formattedResults.forEach(doc -> log.info("Formatted Data: {}", doc.toJson()));
+        Map<String, List<Document>> formattedResults = new HashMap<>();
+        formattedResults.put("RealTime",resultsRT);
+        formattedResults.put("CashFlow",resultsCF);
 
         // Set the formatted results in the body of the exchange
         exchange.getIn().setBody(formattedResults);
+
+        // Log the formatted results
+        //formattedResults.forEach(doc -> log.info("Formatted Data: {}", doc.toJson()));
     }
 }
